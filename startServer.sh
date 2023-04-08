c831f50657818e3693060190bd31495615dca9a5
Petros Koutsolampros <commits@pklampros.io>
2023-04-08 02:01:12 +0100

    Make getting filenames absolute

--- startServer.sh
+++ startServer.sh
@@ -8,12 +8,17 @@ if [ "$#" -ne 1 ]; then
     exit 1
 fi
 
+get_abs_filename() {
+  # $1 : relative filename
+  echo "$(cd "$(dirname "$1")" && pwd)/$(basename "$1")"
+}
+
 PERSISTENT_DATA_ID=$1
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 PROJECT_DIR="$SCRIPT_DIR/../../"
+PROJECT_DIR=$(get_abs_filename "$PROJECT_DIR")
 
-PROJECT_DIR=$(echo "$(cd "$(dirname -- "$1")" >/dev/null; pwd -P)/$(basename -- "$PROJECT_DIR")")
 PERSISTENT_DATA_DIR="$PROJECT_DIR/.nosync/persistent_data/$PERSISTENT_DATA_ID"
 mkdir -p "$PERSISTENT_DATA_DIR"
 
